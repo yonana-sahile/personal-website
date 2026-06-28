c487ccff60668bb0de2bfb7217f479670c2b3088
Yonas Sahile <yonassahile8@gmail.com>
2026-06-28 14:22:34 +0300

    feat: register ContactMessage model in admin

--- personel_backend/web_app/admin.py
+++ personel_backend/web_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import ContactMessage
 
-# Register your models here.
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    readonly_fields = ('created_at',)
+    ordering = ('-created_at',)
