953b95857700ba77178883991a01f6dde57be845
Yonas Sahile <yonassahile8@gmail.com>
2026-06-28 14:03:10 +0300

    feat: add ContactMessage model for contact form submissions

--- personel_backend/web_app/models.py
+++ personel_backend/web_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ContactMessage(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.email} - {self.created_at}'
