42e90fff6be8a98775744bbf27c5ea4d11bb0f8a
Yonas Sahile <yonassahile8@gmail.com>
2026-06-28 14:39:32 +0300

    feat: register Project and CV models in admin

--- personel_backend/web_app/admin.py
+++ personel_backend/web_app/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
-from .models import ContactMessage
+from .models import ContactMessage, Project, CV
 
 @admin.register(ContactMessage)
 class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'created_at')
     readonly_fields = ('created_at',)
     ordering = ('-created_at',)
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'type', 'created_at')
+    readonly_fields = ('created_at',)
+
+@admin.register(CV)
+class CVAdmin(admin.ModelAdmin):
+    list_display = ('id', 'updated_at')
