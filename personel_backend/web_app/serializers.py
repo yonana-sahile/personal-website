a8e2d2a8486d40d6d88086dd604677fd8f4109fc
Yonas Sahile <yonassahile8@gmail.com>
2026-06-29 13:20:41 +0300

    feat: add CertificateSerializer for DRF

--- personel_backend/web_app/serializers.py
+++ personel_backend/web_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Project, CV
+from .models import Project, CV,Certificate
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,4 +10,8 @@ class ProjectSerializer(serializers.ModelSerializer):
 class CVSerializer(serializers.ModelSerializer):
     class Meta:
         model = CV
+        fields = '__all__'
+class CertificateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
         fields = '__all__'
